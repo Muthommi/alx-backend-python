2d0535a84e6e38ba6815d154551329d54fe54b6f
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-11-19 12:18:50 +0300

    Basic annotations-to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A module to feine a type annotated function 'to_str'.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function to return the string representation of a float.
+    Returns: str.
+    """
+    return str(n)
