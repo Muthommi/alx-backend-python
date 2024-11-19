3e998b1f37e6352931214e4cb62f98b4780a47b1
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-11-19 11:51:48 +0300

    Basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module to define a type annotated function.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function to add two float numbers and returns their sum.
+    Returns: float.
+    """
+    return a + b
