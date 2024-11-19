721591110308e3341c65897db075a74b88662793
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-11-19 12:27:17 +0300

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module to define and annotate variables.
+"""
+
+
+a: int = 1
+"""
+Integer with value of 1.
+"""
+
+pi: float = 3.14
+"""
+Float variable with a value of 3.14.
+"""
+
+i_understand_annotations: bool = True
+"""
+Boolean variable with a value of True.
+"""
+
+school: str = "Holberton"
+"""
+String variable with the value of "Holberton".
+"""
